5336b56624e41f8753ad821e2922939e45aa4de2
Brennan <bmart2913@gmail.com>
2017-11-07 01:12:32 -0500

    final

--- NASA_Info_App/src/Grid/Grid.js
+++ NASA_Info_App/src/Grid/Grid.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import GridItem from './GridItem';
+import cx from 'classnames';
 
 import styles from './Grid.css';
+import mediaStyles from './Media.css';
+
+
 
 export default class Grid extends React.Component {
   updateClicked(id){
@@ -18,7 +22,7 @@ export default class Grid extends React.Component {
   }
   render() {
     return (
-      <section className = {styles.grid}>
+      <section className = {cx(styles.grid, mediaStyles.grid)}>
         {this.renderFacts()}
       </section>
     );
